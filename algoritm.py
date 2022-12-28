5c9a39fb36cbc7cf4febd0937305f7fcb515b4ac
Ярослав Безукладников <iaroslavbezukladnikov@MacBook-Pro-Aroslav.local>
2022-12-28 12:10:57 +0300

    algoritm

---
+++ algoritm.py
@@ -0,0 +1,17 @@
+s = 'ffttghhhryttttts'
+
+dict_my = {}
+count = 0
+char = ''
+for i in s:
+    if i in dict_my.keys():
+        dict_my[i] +=1
+        if dict_my[i] > count:
+            count = dict_my[i]
+            char = i
+
+    else:dict_my[i] = 1
+
+
+print(dict_my)
+print(f'{char} = > {count}')
--- modul/Parsing.py
+++ modul/Parsing.py
@@ -2,20 +2,16 @@ from modul.Exception import ExceptionGender
 
 
 class Parsing:
-
     user_data = {}
 
     def get_parsing(self, array):
         for el in array:
             if len(el) == 1: self.parsing_gender(el)
 
-
-
     def parsing_gender(self, simbol):
-        if simbol == 'f': self.user_data['Gender'] = 'f'
-        elif simbol == 'm': self.user_data['Gender'] = 'm'
-        else: raise ExceptionGender("The gender should be entered 'f' or 'm'")
-
-
-
-
+        if simbol == 'f':
+            self.user_data['Gender'] = 'f'
+        elif simbol == 'm':
+            self.user_data['Gender'] = 'm'
+        else:
+            raise ExceptionGender("The gender should be entered 'f' or 'm'")
