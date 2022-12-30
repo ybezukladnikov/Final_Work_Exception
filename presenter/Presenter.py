ee12548d96cab1c4b2fd829660e11db80d6bfbfb
Yaroslav <yaros6666@yandex.ru>
2022-12-30 16:43:32 +0300

    start work

--- presenter/Presenter.py
+++ presenter/Presenter.py
@@ -6,24 +6,26 @@ class Presenter:
     parsing = Parsing()
 
     def start(self):
-        try:
-            self.parsing.get_parsing(['Петров','02.03.1988','3343', 'Иван', 'Иванович', 'f'])
-        except ExceptionGender:
-            print(ExceptionGender.description)
+        my_list = [
+            ['Петров','02.03.1988','3343', 'Иван', 'Иванович', 'f'],
+            ['Петров', '02.03.1988', '3343', 'Иван', 'Иванович', 'm'],
+        ]
+        for i in my_list:
+            try:
+                self.parsing.get_parsing(i)
+            except ExceptionGender:
+                print(ExceptionGender.description)
 
-        except ExceptionDate:
-            print(ExceptionDate.description)
+            except ExceptionDate:
+                print(ExceptionDate.description)
 
-        except ExceptionWrongNumberData:
-            print(ExceptionWrongNumberData.description)
+            except ExceptionWrongNumberData:
+                print(ExceptionWrongNumberData.description)
 
-        except ExceptionNotAllData:
-            print(ExceptionNotAllData.description)
-            for key, value in self.parsing.user_data.items():
-                if value == '':
-                    print(key)
+            except ExceptionNotAllData:
+                print(ExceptionNotAllData.description)
+                for key, value in self.parsing.user_data.items():
+                    if value == '':
+                        print(key)
 
-
-
-
-        print(self.parsing.user_data)
+            print(self.parsing.user_data)
